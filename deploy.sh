8b8bd1629fa137c93067604214326620c686fab6
duyvu871 <78553549+duyvu871@users.noreply.github.com>
2021-10-21 20:39:59 +0700

    New Deployment2

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,8 @@ git add -A
 git commit -m 'New Deployment2'
 # git remote add origin https://github.com/duyvu871/bot-chat-app.git
 # git pull -f origin main
-# git fetch https://github.com/duyvu871/bot-chat-app.git main
+# git fetch https://github.com/duyvu871/bot-chat-app.git ma
+
 git pull https://github.com/duyvu871/bot-chat-app.git main --allow-unrelated-histories
 
 cd -
