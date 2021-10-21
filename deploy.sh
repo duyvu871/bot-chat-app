705324b0e7169ea786843003a580a42c260c8c3a
duyvu871 <78553549+duyvu871@users.noreply.github.com>
2021-10-21 20:49:10 +0700

    New Deployment2

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,6 @@ cd dist
 # git init
 git add -A
 git commit -m 'New Deployment2'
-git remote add second https://github.com/duyvu871/bot-chat-app.git
 # git pull -f origin main
 # git fetch https://github.com/duyvu871/bot-chat-app.git ma
 
